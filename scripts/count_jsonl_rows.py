c12a04fb6ad3eea285e7d433842ac0e66bbf6cf1
Christian F. Howard <58192645+TwitLit@users.noreply.github.com>
2020-03-02 12:47:00 -0500

    Update to work with Python3

--- scripts/count_jsonl_rows.py
+++ scripts/count_jsonl_rows.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import json_lines
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             for item in json_lines.reader(file):
                 count = count + 1
 
-        print "number of rows in file: " + str(count)
+        print('number of rows in file: {}').format(count)
         file.close()
 
     except:
